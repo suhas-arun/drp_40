44bedd5180776054afe731bc0ce8fdf3f5383fa7
Ben James <bj420@ic.ac.uk>
2023-06-08 17:42:21 +0100

    Changed top_bar to an AppBar from container

--- lib/components/top_bar.dart
+++ lib/components/top_bar.dart
@@ -1,44 +1,45 @@
-import 'package:saveshare/constants/size.dart';
 import 'package:flutter/material.dart';
 
 import '../constants/colour.dart';
+import '../constants/size.dart';
 
-class TopBar extends StatefulWidget {
-  const TopBar({super.key});
+class TopBar extends StatefulWidget implements PreferredSizeWidget {
+  TopBar({Key? key})
+      : preferredSize = const Size.fromHeight(kToolbarHeight),
+        super(key: key);
 
   @override
-  State<TopBar> createState() => _TopBarState();
+  final Size preferredSize;
+
+  @override
+  State<StatefulWidget> createState() => _TopBarState();
 }
 
-class _TopBarState extends State<TopBar> {
+class _TopBarState extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: APPColour.green,
-        height: APPSize.BAR_HEIGHT(context),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              alignment: Alignment.centerLeft,
-              iconSize: APPSize.APP_ICON_SIZE,
-              color: Colors.white,
-              icon: const Icon(Icons.menu_outlined),
-              onPressed: () {},
-            ),
-            IconButton(
-              iconSize: APPSize.APP_ICON_SIZE,
-              color: Colors.white,
-              icon: const Icon(Icons.energy_savings_leaf_outlined),
-              onPressed: () {},
-            ),
-            IconButton(
-              iconSize: APPSize.APP_ICON_SIZE,
-              color: APPColour.green,
-              icon: const Icon(Icons.notifications_none_outlined),
-              onPressed: () {},
-            ),
-          ],
-        ));
+    return AppBar(
+        backgroundColor: APPColour.green,
+        leading: IconButton(
+          alignment: Alignment.centerLeft,
+          iconSize: APPSize.APP_ICON_SIZE,
+          color: Colors.white,
+          icon: const Icon(Icons.menu_outlined),
+          onPressed: () {},
+        ),
+        title: IconButton(
+          iconSize: APPSize.APP_ICON_SIZE,
+          color: Colors.white,
+          icon: const Icon(Icons.energy_savings_leaf_outlined),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            iconSize: APPSize.APP_ICON_SIZE,
+            color: Colors.white,
+            icon: const Icon(Icons.notifications_none_outlined),
+            onPressed: () {},
+          )
+        ]);
   }
 }
