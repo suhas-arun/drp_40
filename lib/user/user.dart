7e49dc674b13cbd8bbc6479a1fd08f23f06f54b8
sa2021 <sa2021@ic.ac.uk>
2023-06-08 17:52:28 +0100

    Added db support for loading user data

--- lib/user/user.dart
+++ lib/user/user.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:saveshare/constants/size.dart';
 import 'package:flutter/material.dart';
 
@@ -22,9 +23,19 @@ class User {
   //will have to change this to a picture
   Icon profilePicture;
 
-  static List<User> getTestUsers() {
+  static Future<List<User>> getTestUsers() async {
+    List<User> userList = List.empty();
+    QuerySnapshot<Map<String, dynamic>> userSnapshot =
+        await FirebaseFirestore.instance.collection("user").get();
+    for (var doc in userSnapshot.docs) {
+      Map<String, dynamic> user = doc.data();
+      print(user);
+      // userList.add(
+      //     User(name: user["name"], energyPercentage: user["energyPercentage"]));
+    }
+    // TODO: FIX SO ADD TO LIST DOESN'T HANG.
     return List.of([
-      User(name: "Marcus!", energyPercentage: (34)),
+      User(name: "Marcus", energyPercentage: (34)),
       User(name: "Alex", energyPercentage: (25)),
       User(name: "Ella", energyPercentage: (17)),
       User(name: "Ines", energyPercentage: (24)),
