0a4a00c478d4928494c1bdfa106df7a8e5a56c66
Ben James <bj420@ic.ac.uk>
2023-06-09 11:40:48 +0100

    Added gesture detector to allow user to quit off keyboard

--- lib/pages/shower_page.dart
+++ lib/pages/shower_page.dart
@@ -18,65 +18,74 @@ class _AddShowerPageState extends State<AddShowerPage> {
   final TextEditingController _timecontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.white,
-        body: Column(children: [
-          const TopBar(),
-          Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Row(children: [
-                const Padding(
-                    padding: EdgeInsets.only(top: 30),
-                    child: Icon(Icons.shower_outlined,
-                        color: APPColour.green, size: APPSize.LARGE_ICON_SIZE)),
-                Expanded(child: Container()),
-                const Padding(
-                    padding: EdgeInsets.only(top: 30),
-                    child: Text("Add Shower", style: APPText.LARGE_TEXT)),
-                Expanded(child: Container())
-              ])),
-          Expanded(child: Container()),
-          Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.85,
-                child: TextField(
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    controller: _timecontroller,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Enter duration (minutes)',
-                        hintText: 'Duration')),
-              )),
-          CheckboxListTile(
-            contentPadding: const EdgeInsets.only(left: 20),
-            title: const Text("Cold Shower", style: APPText.LARGE_TEXT),
-            value: checkedValue,
-            onChanged: (newValue) {
-              setState(() {
-                checkedValue = newValue!;
-              });
-            },
-            controlAffinity:
-                ListTileControlAffinity.leading, //  <-- leading Checkbox
-          ),
-          Expanded(child: Container()),
-          ElevatedButton(
-              onPressed: () {
-                double duration = double.parse(_timecontroller.text);
-                //remember checkedValue is a bool that is true if Cold Shower
-                //add firebase section here
-              },
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(13)),
-                  backgroundColor: APPColour.green,
-                  fixedSize: const Size(200, 50)),
-              child: const Text('Log Action', style: APPText.LARGE_WHITE_TEXT)),
-          Expanded(child: Container()),
-          const BottomBar(),
-        ]));
+    return GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.white,
+            body: Column(children: [
+              const TopBar(),
+              Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Row(children: [
+                    const Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: Icon(Icons.shower_outlined,
+                            color: APPColour.green,
+                            size: APPSize.LARGE_ICON_SIZE)),
+                    Expanded(child: Container()),
+                    const Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: Text("Add Shower", style: APPText.LARGE_TEXT)),
+                    Expanded(child: Container())
+                  ])),
+              Expanded(child: Container()),
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    child: TextField(
+                        keyboardType: const TextInputType.numberWithOptions(
+                            decimal: true),
+                        controller: _timecontroller,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter duration (minutes)',
+                            hintText: 'Duration')),
+                  )),
+              CheckboxListTile(
+                contentPadding: const EdgeInsets.only(left: 20),
+                title: const Text("Cold Shower", style: APPText.LARGE_TEXT),
+                value: checkedValue,
+                onChanged: (newValue) {
+                  setState(() {
+                    checkedValue = newValue!;
+                  });
+                },
+                controlAffinity:
+                    ListTileControlAffinity.leading, //  <-- leading Checkbox
+              ),
+              Expanded(child: Container()),
+              ElevatedButton(
+                  onPressed: () {
+                    double duration = double.parse(_timecontroller.text);
+                    //remember checkedValue is a bool that is true if Cold Shower
+                    //add firebase section here
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(13)),
+                      backgroundColor: APPColour.green,
+                      fixedSize: const Size(200, 50)),
+                  child: const Text('Log Action',
+                      style: APPText.LARGE_WHITE_TEXT)),
+              Expanded(child: Container()),
+              const BottomBar(),
+            ])));
   }
 }
