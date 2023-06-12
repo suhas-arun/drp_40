74db91ed16079c1833858457eb05bc73037deba7
Ben James <bj420@ic.ac.uk>
2023-06-12 22:53:26 +0100

    Added minutes comparison over time for personal breakdown

--- lib/pages/breakdown_page.dart
+++ lib/pages/breakdown_page.dart
@@ -3,11 +3,12 @@ import 'package:pie_chart/pie_chart.dart';
 import 'package:saveshare/constants/actions.dart';
 import 'package:saveshare/constants/buttons.dart';
 import 'package:saveshare/constants/colour.dart';
-import 'package:saveshare/constants/size.dart';
+import 'package:vertical_percent_indicator/vertical_percent_indicator.dart';
 import 'package:saveshare/constants/text.dart';
 
 import '../components/bottom_bar.dart';
 import '../components/top_bar.dart';
+import '../constants/size.dart';
 
 class BreakdownPage extends StatefulWidget {
   const BreakdownPage({super.key, required this.curUser});
@@ -33,7 +34,7 @@ class _BreakdownPageState extends State<BreakdownPage> {
             const TopBar(),
             titleSection(context),
             _actions(),
-            Expanded(child: Container(child: actionBreakdown(context))),
+            actionBreakdown(context),
             const BottomBar(),
           ],
         ));
@@ -41,11 +42,14 @@ class _BreakdownPageState extends State<BreakdownPage> {
 
   Widget actionBreakdown(context) {
     if (selectedIndex == 0) {
-      return Column(children: [showerStats(), coldShowerBreakdown()]);
+      return Expanded(
+        child: ListView(
+            children: [minutesComparisonCharts(), coldShowerBreakdown()]),
+      );
     } else if (selectedIndex == 1) {
-      return Column(children: []);
+      return Expanded(child: ListView());
     } else {
-      return Column(children: []);
+      return Expanded(child: ListView());
     }
   }
 
@@ -54,7 +58,8 @@ class _BreakdownPageState extends State<BreakdownPage> {
         padding: const EdgeInsets.all(20),
         child: Title(
             color: APPColour.green,
-            child: const Text("Your breakdown", style: APPText.LARGE_TEXT)));
+            child: const Text("Hi Alex, here is your personal breakdown:",
+                style: APPText.LARGE_TEXT)));
   }
 
   _actions() {
@@ -64,7 +69,7 @@ class _BreakdownPageState extends State<BreakdownPage> {
           borderRadius: BorderRadius.all(Radius.circular(5.0))),
       width: double.infinity,
       height: 26,
-      margin: const EdgeInsets.all(10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
       child: Row(children: <Widget>[
         for (var action in APPActions.actions)
           Expanded(
@@ -86,101 +91,104 @@ class _BreakdownPageState extends State<BreakdownPage> {
     );
   }
 
-  Widget showerStats() {
-    return Container(
-        margin: const EdgeInsets.all(10),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(children: [
-              const Text("This week", style: APPText.MEDIUM_TEXT),
-              Expanded(child: Container())
-            ]),
-            Row(children: [
-              const Text("Your daily average"),
-              Expanded(child: Container()),
-              const Text("4 minutes")
-            ]),
-            Row(children: [
-              const Text("House daily average"),
-              Expanded(child: Container()),
-              const Text("6 minutes")
-            ]),
-            Row(children: [
-              const Text("Compared to house average"),
-              Expanded(child: Container()),
-              const Text("2 minutes less", style: APPText.GOOD_TEXT)
-            ]),
-            Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: Row(children: [
-                const Text("This Month", style: APPText.MEDIUM_TEXT),
-                Expanded(child: Container()),
-              ]),
-            ),
-            Row(children: [
-              const Text("Your daily average"),
-              Expanded(child: Container()),
-              const Text("6")
-            ]),
-            Row(children: [
-              const Text("House daily average"),
-              Expanded(child: Container()),
-              const Text("5")
-            ]),
-            Row(children: [
-              const Text("Compared to house average"),
-              Expanded(child: Container()),
-              const Text("1 minute longer", style: APPText.OK_TEXT)
-            ]),
-            Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: Row(children: [
-                const Text("Your Average", style: APPText.MEDIUM_TEXT),
-                Expanded(child: Container()),
-              ]),
-            ),
-            Row(children: [
-              const Text("Your daily average"),
-              Expanded(child: Container()),
-              const Text("5 minutes")
-            ]),
-            Row(children: [
-              const Text("House daily average"),
-              Expanded(child: Container()),
-              const Text("5 minutes")
-            ]),
-            Row(children: [
-              const Text("Compared to house average"),
-              Expanded(child: Container()),
-              const Text("0 minutes longer", style: APPText.OK_TEXT)
-            ])
-          ],
-        ));
-  }
-
   Widget coldShowerBreakdown() {
-    return SizedBox(
-        child: Column(children: [coldShowerTitle, coldShowerChart]));
+    return Column(children: [coldShowerTitle(), coldShowerChart()]);
   }
 
-  Widget coldShowerTitle = Container(
-      width: double.infinity,
-      alignment: Alignment.center,
-      padding: const EdgeInsets.only(bottom: 25),
-      child: const Text("Cold Showers", style: APPText.MEDIUM_TEXT));
+  Widget coldShowerTitle() {
+    return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.symmetric(horizontal: 30),
+        padding: const EdgeInsets.only(bottom: 25),
+        child: const Text("You have had 1 cold shower this month:",
+            style: APPText.MEDIUM_TEXT));
+  }
 
-  Widget coldShowerChart = Container(
-    padding: const EdgeInsets.symmetric(horizontal: 50),
-    child: PieChart(
-        dataMap: const {"Cold": 1, "Hot": 18},
-        chartType: ChartType.ring,
-        baseChartColor: Colors.grey[50]!.withOpacity(0.15),
-        colorList: const [Colors.blueAccent, Colors.redAccent],
-        chartValuesOptions: const ChartValuesOptions(
-          showChartValuesInPercentage: true,
-        ),
-        totalValue: 19),
-  );
+  Widget coldShowerChart() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: PieChart(
+          dataMap: const {"Cold": 1, "Hot": 18},
+          chartType: ChartType.ring,
+          baseChartColor: Colors.grey[50]!.withOpacity(0.15),
+          colorList: const [Colors.blueAccent, Colors.redAccent],
+          chartValuesOptions: const ChartValuesOptions(
+            showChartValuesInPercentage: true,
+          ),
+          totalValue: 19),
+    );
+  }
+
+  Widget minutesComparisonCharts() {
+    return Container(
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 30),
+        padding: const EdgeInsets.only(bottom: 30),
+        child: Column(children: [
+          const Padding(
+            padding: EdgeInsets.only(bottom: 25),
+            child: Text(
+                "This is how many minutes you've spent showering on average (daily) :",
+                style: APPText.MEDIUM_TEXT),
+          ),
+          Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Expanded(
+              child: VerticalBarIndicator(
+                percent: 0.7,
+                header: "5.3",
+                animationDuration: const Duration(seconds: 1),
+                height: APPSize.REM_HEIGHT(context) / 3 - 80,
+                width: 30,
+                color: const [
+                  Colors.indigo,
+                  Colors.teal,
+                ],
+                footer: 'All Time',
+              ),
+            ),
+            Expanded(
+              child: VerticalBarIndicator(
+                percent: 0.6,
+                header: "4.6",
+                animationDuration: const Duration(seconds: 1),
+                height: APPSize.REM_HEIGHT(context) / 3 - 80,
+                width: 30,
+                color: const [
+                  Colors.indigo,
+                  Colors.teal,
+                ],
+                footer: 'Year',
+              ),
+            ),
+            Expanded(
+              child: VerticalBarIndicator(
+                percent: 0.65,
+                header: "5.0",
+                animationDuration: const Duration(seconds: 1),
+                height: APPSize.REM_HEIGHT(context) / 3 - 80,
+                width: 30,
+                color: const [
+                  Colors.indigo,
+                  Colors.teal,
+                ],
+                footer: 'Month',
+              ),
+            ),
+            Expanded(
+              child: VerticalBarIndicator(
+                percent: 0.5,
+                header: "4.2",
+                animationDuration: const Duration(seconds: 1),
+                height: APPSize.REM_HEIGHT(context) / 3 - 80,
+                width: 30,
+                color: const [
+                  Colors.indigo,
+                  Colors.teal,
+                ],
+                footer: 'Week',
+              ),
+            ),
+          ])
+        ]));
+  }
 }
