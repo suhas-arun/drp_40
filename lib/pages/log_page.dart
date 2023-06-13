623fd2a167c606849adcb8f0b5dc5ec4bbcd5a46
Ben James <bj420@ic.ac.uk>
2023-06-13 15:59:37 +0100

    Added action-specific text to logging buttons

--- lib/pages/log_page.dart
+++ lib/pages/log_page.dart
@@ -95,23 +95,82 @@ class _LogPageState extends State<LogPage> {
                                 fit: BoxFit.fill,
                                 image: AssetImage(
                                     'assets/images/$picName.png')))))),
-            Positioned(
-                top: 70,
-                left: APPSize.WIDTH(context) * (rightAlign ? 0.6 : 0.1),
-                child: SizedBox(
-                    width: APPSize.WIDTH(context) * 0.375,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Log a Shower", style: APPText.mediumText(colour)),
-                        const Divider(),
-                        const Text("Tell us your:"),
-                        const Text("  - Shower length"),
-                        const Text("  - Hot or cold")
-                      ],
-                    )))
+            logActionText(rightAlign, colour, picName)
           ],
         ));
+  }
+
+  Widget logActionText(rightAlign, colour, actionString) {
+    if (actionString == "shower") {
+      return logShowerText(rightAlign, colour);
+    } else if (actionString == "laundry") {
+      return logLaundryText(rightAlign, colour);
+    } else {
+      return logHeatingText(rightAlign, colour);
+    }
+  }
+
+  Widget logShowerText(rightAlign, colour) {
+    return Positioned(
+        top: 70,
+        left: APPSize.WIDTH(context) * (rightAlign ? 0.6 : 0.1),
+        child: SizedBox(
+            width: APPSize.WIDTH(context) * 0.325,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Log a Shower", style: APPText.mediumText(colour)),
+                const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 13),
+                    child: Divider(thickness: 2.0)),
+                const Text("Tell us your:"),
+                const Padding(padding: EdgeInsets.only(bottom: 5)),
+                const Text("  - Shower length"),
+                const Text("  - Temperature")
+              ],
+            )));
+  }
+
+  Widget logHeatingText(rightAlign, colour) {
+    return Positioned(
+        top: 70,
+        left: APPSize.WIDTH(context) * (rightAlign ? 0.6 : 0.1),
+        child: SizedBox(
+            width: APPSize.WIDTH(context) * 0.325,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Log Heating", style: APPText.mediumText(colour)),
+                const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 13),
+                    child: Divider(thickness: 2.0)),
+                const Text("Tell us if you:"),
+                const Padding(padding: EdgeInsets.only(bottom: 5)),
+                const Text("  - Changed room"),
+                const Text("    temperature"),
+              ],
+            )));
+  }
+
+  Widget logLaundryText(rightAlign, colour) {
+    return Positioned(
+        top: 70,
+        left: APPSize.WIDTH(context) * (rightAlign ? 0.6 : 0.1),
+        child: SizedBox(
+            width: APPSize.WIDTH(context) * 0.325,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Log Laundry", style: APPText.mediumText(colour)),
+                const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 13),
+                    child: Divider(thickness: 2.0)),
+                const Text("Did you:"),
+                const Padding(padding: EdgeInsets.only(bottom: 5)),
+                const Text("  - Use Eco mode"),
+                const Text("  - Hand dry")
+              ],
+            )));
   }
 
   Widget logShower(BuildContext context) {
