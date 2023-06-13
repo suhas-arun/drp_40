bf785f9e2a2636cdd4befccb816734dd308ca902
Ben James <bj420@ic.ac.uk>
2023-06-13 11:37:33 +0100

    Refactored logging shower action

--- lib/pages/log_page.dart
+++ lib/pages/log_page.dart
@@ -4,6 +4,7 @@ import 'package:saveshare/pages/shower_page.dart';
 
 import '../components/bottom_bar.dart';
 import '../components/top_bar.dart';
+
 import '../constants/size.dart';
 import '../constants/text.dart';
 
@@ -19,32 +20,37 @@ class _LogPageState extends State<LogPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.white,
         body: Column(
           children: [
             const TopBar(),
-            Expanded(child: Container()),
-            Row(children: [
-              IconButton(
-                padding: const EdgeInsets.all(40.0),
-                alignment: Alignment.centerLeft,
-                iconSize: APPSize.LARGE_ICON_SIZE,
-                color: APPColour.green,
-                icon: const Icon(Icons.shower_outlined),
-                splashRadius: APPSize.LARGE_ICON_SPLASH,
-                onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const AddShowerPage()),
-                  );
-                },
-              ),
-              const Text("Log a Shower", style: APPText.LARGE_TEXT)
-            ]),
-            Expanded(child: Container()),
+            Expanded(child: actionPager()),
             const BottomBar(),
           ],
         ));
+  }
+
+  Widget actionPager() {
+    return Container();
+  }
+
+  Widget logShower(BuildContext context) {
+    return Row(children: [
+      IconButton(
+        padding: const EdgeInsets.all(40.0),
+        alignment: Alignment.centerLeft,
+        iconSize: APPSize.LARGE_ICON_SIZE,
+        color: APPColour.green,
+        icon: const Icon(Icons.shower_outlined),
+        splashRadius: APPSize.LARGE_ICON_SPLASH,
+        onPressed: () {
+          Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddShowerPage()),
+          );
+        },
+      ),
+      const Text("Log a Shower", style: APPText.LARGE_TEXT)
+    ]);
   }
 }
