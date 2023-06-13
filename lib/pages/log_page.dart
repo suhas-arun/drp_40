d4358d690b7d635765212c96d1282e37fdd5784a
Ben James <bj420@ic.ac.uk>
2023-06-13 19:09:09 +0100

    Added action images to top section of logging page

--- lib/pages/log_page.dart
+++ lib/pages/log_page.dart
@@ -33,13 +33,32 @@ class _LogPageState extends State<LogPage> {
   Widget topSection(String actionName) {
     return SizedBox(
         width: APPSize.WIDTH(context),
-        height: APPSize.REM_HEIGHT(context) * 0.4,
+        height: APPSize.REM_HEIGHT(context) * 0.5,
         child: Stack(children: [
           Positioned(
               top: 20,
               left: 10,
               child: APPButtons.backButton(
-                  APPColour.actionColour(actionName), context))
+                  APPColour.actionColour(actionName), context)),
+          Positioned(
+            top: APPSize.REM_HEIGHT(context) * 0.05,
+            left: APPSize.WIDTH(context) * 0.15,
+            child: Card(
+                elevation: 0,
+                color: Colors.transparent,
+                //shadowColor: Colors.grey.withOpacity(0.5),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0)),
+                child: Container(
+                    height: APPSize.WIDTH(context) * 0.7,
+                    width: APPSize.WIDTH(context) * 0.7,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image:
+                                AssetImage('assets/images/$actionName.png'))))),
+          ),
         ]));
   }
 }
