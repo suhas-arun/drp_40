955dce5565c25cc0a83cd50aa7cdf4974951088b
Ben James <bj420@ic.ac.uk>
2023-06-15 00:59:26 +0100

    Added image to home page and refactored user tiles

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../constants/colour.dart';
 import '../user/user.dart';
 
 import '../constants/size.dart';
@@ -21,31 +22,47 @@ class UserTile extends StatelessWidget {
                   energyPercentage:
                       (duration / User.householdShowerDuration).toDouble()));
             });
-            return SizedBox(
-                height: APPSize.REM_HEIGHT(context),
-                child: ListView.builder(
-                  itemCount: users.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: users[index].profilePicture,
-                      title: Text(users[index].name),
-                      subtitle: Text(
-                          "Energy Share: ${(users[index].energyPercentage * 100).roundToDouble()}%"),
-                      onTap: () {
-                        const curUser = User.curUser;
-                        if (curUser == users[index].name) {
-                          Navigator.pop(context);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const BreakdownPage(curUser: User.curUser)),
-                          );
-                        }
-                      },
-                    );
-                  },
-                ));
+            return Expanded(
+              child: SizedBox(
+                  width: APPSize.WIDTH(context) * 0.9,
+                  child: ListView.builder(
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      bool isCurUser = users[index].name == User.curUser;
+                      return Material(
+                        child: Container(
+                          margin: const EdgeInsets.only(bottom: 15),
+                          decoration: BoxDecoration(
+                            color: APPColour.laundryGreen.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            leading: users[index].profilePicture,
+                            trailing: isCurUser
+                                ? const Icon(Icons.arrow_forward_ios_outlined)
+                                : null,
+                            title: Text(users[index].name +
+                                (isCurUser ? " (You)" : "")),
+                            subtitle: Text(
+                                "Energy Share: ${(users[index].energyPercentage * 100).roundToDouble()}%"),
+                            onTap: () {
+                              const curUser = User.curUser;
+                              if (curUser == users[index].name) {
+                                Navigator.pop(context);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const BreakdownPage(
+                                          curUser: User.curUser)),
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  )),
+            );
           } else {
             return const CircularProgressIndicator();
           }
